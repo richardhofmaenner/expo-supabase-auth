2e88819157cb16587755ff07acd56c7d92cc1539
Richard Hofmänner <hello@terillos.dev>
2023-02-25 11:35:48 +0100

    Moved secrets to .env

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,8 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Better put your these secret keys in .env file
 export const supabaseClient = createClient(
-  "http://localhost:54321",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
   {
     // @ts-ignore
     localStorage: AsyncStorage as any,
