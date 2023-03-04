2c02de36aa45c064449767d00dee3237c3d67f00
Richard Hofmänner <hello@terillos.dev>
2023-03-04 17:57:38 +0100

    Fixed Supabase URL

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,3 +1,4 @@
+import 'react-native-url-polyfill/auto'
 import AsyncStorage from "@react-native-async-storage/async-storage";
 //import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
@@ -5,8 +6,8 @@ import Constants from "expo-constants";
 
 // Better put your these secret keys in .env file
 export const supabaseClient = createClient(
-  Constants.expoConfig.extra.supabaseURL,
-  Constants.expoConfig.extra.supabaseKey,
+  "https://6cdb-85-195-229-16.eu.ngrok.io",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
   {
     // @ts-ignore
     localStorage: AsyncStorage as any,
